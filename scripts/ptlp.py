c9c7796be10c2fc1779d3c57f19a7542ff67feb3
ombuvirtual <39426197+ombuvirtual@users.noreply.github.com>
2019-11-21 07:31:50 +1100

    Updated 02_minimal.py
    Added section 'comparison operators'.

--- scripts/ptlp.py
+++ scripts/ptlp.py
@@ -4,3 +4,7 @@ from inspect import currentframe, getframeinfo
 def ln():
     """Gets current line number in source code"""
     return str(currentframe().f_back.f_lineno) + ">"
+
+def l():
+    """Gets current line number in source code"""
+    return str(currentframe().f_back.f_lineno) + ">"
